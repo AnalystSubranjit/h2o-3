e41472ba365c9eaf5377d4ab839dbfdb92735edd
Jan Sterba <jan.sterba@h2o.ai>
2019-02-22 15:33:33 +0100

    [HEXDEV-720] update docker images for easier hive import testing

--- docker/hadoop/hdp/scripts/sbin/install_hive.sh
+++ docker/hadoop/hdp/scripts/sbin/install_hive.sh
@@ -15,15 +15,4 @@ rm apache-hive-${HIVE_VERSION}-bin.tar.gz
 chown -R hive:hive apache-hive-*/
 sudo -E -u hive ${HIVE_HOME}/bin/schematool -dbType derby -initSchema
 
-mkdir -p /opt/hive-jars/
-cp ${HIVE_HOME}/lib/hive-jdbc-* /opt/hive-jars/
-cp ${HIVE_HOME}/lib/hive-service-* /opt/hive-jars/
-cp ${HIVE_HOME}/lib/hive-metastore-* /opt/hive-jars/
-cp ${HIVE_HOME}/lib/hive-common-* /opt/hive-jars/
-cp ${HIVE_HOME}/lib/hive-cli-* /opt/hive-jars/
-cp ${HIVE_HOME}/lib/hive-exec-* /opt/hive-jars/
-cp ${HIVE_HOME}/lib/libfb303-* /opt/hive-jars/
-cp ${HIVE_HOME}/lib/libthrift-* /opt/hive-jars/
-
-find /opt/hive-jars/ -name '*.jar' | tr '\n' ',' > /opt/hive-jars/hive-libjars
-
+ls $HIVE_HOME/lib | grep 'jdbc' | tr '\n' ':' > /opt/hive-jdbc-cp
